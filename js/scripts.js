6c623a6a8605e4642f8998e28181368850745128
justine <jgalecia.vb@gmail.com>
2025-04-25 14:30:43 +0800

    --

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
 });
+
+const slider = document.querySelector('.slider');
+
+function activate(e) {
+  const items = document.querySelectorAll('.item');
+  e.target.matches('.next') && slider.append(items[0])
+  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+}
+
+document.addEventListener('click',activate,false);
